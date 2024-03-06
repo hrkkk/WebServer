2bd7e393f447f8b54307563bdf0352ed850d38bc
hrkkk <hongrk@foxmail.com>
2024-03-06 22:55:37 +0800

    update thread pool

--- ThreadPool.cpp
+++ ThreadPool.cpp
@@ -3,179 +3,81 @@
 //
 
 #include "ThreadPool.h"
-#include <cstdlib>
-#include <iostream>
-#include <cstring>
 
-using namespace std;
-const int NUMBER = 2;
-
-ThreadPool::ThreadPool(int min, int max)
+ThreadPool::ThreadPool(int num) : m_shutdown(false)
 {
-    m_minNum = min;
-    m_maxNum = max;
-    m_busyNum = 0;
-    m_liveNum = min;
-    m_exitNum = 0;
-    m_shutdown = false;
-    // 创建管理者线程
-    m_managerID = thread(manager, this);
-    // 创建工作线程
-    m_workThreads.resize(max);
-    for (int i = 0; i < min; ++i) {
-        m_workThreads[i] = thread(worker, this);
+    for (size_t i = 0; i < num; ++i) {
+        // emplace_back会创建thread对象，相当于创建线程，传递工作函数
+        m_workThreads.emplace_back([this] {
+            while (1) {
+                std::function<void()> task;
+
+                {
+                    std::unique_lock<std::mutex> lock(m_mutex);
+                    // 任务队列为空时（且线程池未关闭），当前工作线程阻塞，等待唤醒
+                    m_condition.wait(lock, [this] {
+                        return m_shutdown || !m_taskQueue.empty();
+                    });
+                    // 如果线程池关闭且任务队列为空，销毁当前线程
+                    if (m_shutdown && m_taskQueue.empty()) {
+                        return;
+                    }
+
+                    // 从任务队列中取出第一个任务
+                    task = std::move(m_taskQueue.front());
+                    m_taskQueue.pop();
+                }
+
+                // 执行任务
+                task();
+            }
+        });
     }
 }
 
 ThreadPool::~ThreadPool()
 {
-    m_shutdown = true;
-    // 阻塞回收管理者线程
-    if (m_managerID.joinable()) {
-        m_managerID.join();
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+        m_shutdown = true;
     }
-    // 唤醒阻塞的消费者线程
+    // 通知所有空闲线程自行销毁
     m_condition.notify_all();
-    for (int i = 0; i < m_maxNum; ++i) {
-        if (m_workThreads[i].joinable()) {
-            m_workThreads[i].join();
-        }
+    // 等待所有线程中的任务都执行完成后线程池才能真正的关闭
+    for (std::thread& worker : m_workThreads) {
+        worker.join();
     }
 }
 
-void ThreadPool::addTask(Task task)
+template<class F, class... Args>
+auto ThreadPool::addTask(F &&f, Args &&...args)
+    -> std::future<typename std::result_of<F(Args...)>::type>
 {
-    unique_lock<mutex> lk(m_mutexPool);
-    if (m_shutdown) {
-        return;
+    // 获取添加的任务函数的返回类型
+    using return_type = typename std::result_of<F(Args...)>::type;
+
+    auto task = std::make_shared<std::packaged_task<return_type()>>(
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+            );
+
+    // 绑定任务函数的返回值
+    std::future<return_type> res = task->get_future();
+
+    {
+        std::unique_lock<std::mutex> lock(m_mutex);
+
+        // 如果添加任务时线程池已经关闭，则抛出异常
+        if (m_shutdown) {
+            throw std::runtime_error("Add task on stopped ThreadPool");
+        }
+
+        // 将任务添加到任务队列中
+        m_taskQueue.emplace([task]() { (*task)(); });
     }
-    // 添加任务
-    m_taskQueue.push(task);
-    m_condition.notify_all();
+
+    // 通知一个空闲中的工作线程
+    m_condition.notify_one();
+    // 返回任务函数的返回值，调用方可以通过绑定future获取返回值
+    return res;
 }
 
-void ThreadPool::addTask(callback func, void *arg)
-{
-    addTask(Task(func, arg));
-}
-
-int ThreadPool::busyNum()
-{
-    m_mutexPool.lock();
-    int busy = m_busyNum;
-    m_mutexPool.unlock();
-    return busy;
-}
-
-int ThreadPool::aliveNum()
-{
-    m_mutexPool.lock();
-    int alive = m_liveNum;
-    m_mutexPool.unlock();
-    return alive;
-}
-
-void ThreadPool::worker(void *arg)
-{
-    ThreadPool* pool = static_cast<ThreadPool*>(arg);
-    // 工作者线程需要不停的获取线程池任务队列，所以使用while
-    while (true) {
-        // 每一个线程都需要对线程池进行任务队列操作，因此线程池是共享资源，需要加锁
-        unique_lock<mutex> lk(pool->m_mutexPool);
-        // 当前任务队列是否为空
-        while (pool->m_taskQueue.empty() && !pool->m_shutdown) {
-            // 如果任务队列为空，并且线程池没有被关闭，则阻塞当前工作线程，等待其他线程通知或有任务加入
-            pool->m_condition.wait(lk);
-
-            // 判断是否要销毁线程，管理者让该工作者线程自杀
-            if (pool->m_exitNum > 0) {
-                pool->m_exitNum--;
-                if (pool->m_liveNum > pool->m_minNum) {
-                    pool->m_liveNum--;
-                    cout << "Thread: " << this_thread::get_id() << " exit..." << endl;
-                    // 当前线程拥有互斥锁，需要解锁，否则会死锁
-                    lk.unlock();
-                    return;
-                }
-            }
-        }
-        // 判断线程池是否关闭了
-        if (pool->m_shutdown) {
-            cout << "Thread: " << this_thread::get_id() << " exit..." << endl;
-            return;
-        }
-
-        // 以下是工作流程：
-        // 从任务队列中取出一个任务
-        Task task = pool->m_taskQueue.front();
-        pool->m_taskQueue.pop();
-        pool->m_busyNum++;
-        // 当访问完线程池队列时，线程池解锁
-        lk.unlock();
-
-        // 取出Task任务后，就可以在当前线程中执行该任务了
-        cout << "Thread: " << this_thread::get_id() << " start work..." << endl;
-        task.m_function(task.m_arg);
-        free(task.m_arg);
-        task.m_arg = nullptr;
-
-        // 任务执行完毕，忙线程解锁，线程回到空闲状态
-        cout << "Thread: " << this_thread::get_id() << " end work..." << endl;
-        lk.lock();
-        pool->m_busyNum--;
-        lk.unlock();
-    }
-}
-
-void ThreadPool::manager(void *arg)
-{
-    ThreadPool* pool = static_cast<ThreadPool*>(arg);
-    // 管理者线程需要不停的监视线程池队列和工作线程
-    while (!pool->m_shutdown) {
-        // 每隔3秒检测一次
-        this_thread::sleep_for(chrono::seconds(3));
-
-        // 取出线程池中任务的数量和当前线程的数量，别的线程有可能在写数据，所以需要加锁
-        // 目的是添加或销毁线程
-        unique_lock<mutex> lk(pool->m_mutexPool);
-        int queueSize = pool->m_taskQueue.size();
-        int liveNum = pool->m_liveNum;
-        int busyNum = pool->m_busyNum;
-        lk.unlock();
-
-        // 添加线程 —— 代表当前工作线程不够，且未达到最大线程数
-        // 任务的个数 > 存活的线程个数 && 存活的线程数 < 最大线程数
-        if (queueSize > liveNum && liveNum < pool->m_maxNum) {
-            // 因为在for循环中操作了线程池变量，所以需要加锁
-            lk.lock();
-            // 用于计数，添加的线程个数
-            int count = 0;
-            // 添加线程
-            for (int i = 0; i < pool->m_maxNum && count < NUMBER && pool->m_liveNum < pool->m_maxNum; ++i) {
-                // 判断当前线程ID，用来存储创建的线程ID
-                if (pool->m_workThreads[i].get_id() == thread::id()) {
-                    cout << "Create a new thread..." << endl;
-                    pool->m_workThreads[i] = thread(worker, pool);
-                    // 线程创建完毕
-                    count++;
-                    pool->m_liveNum++;
-                }
-            }
-            lk.unlock();
-        }
-
-        // 销毁线程 —— 当前存活的线程太多了，工作的线程太少了
-        // 忙的线程*2 < 存活的线程数 && 存活的线程数 > 最小的线程数
-        if (busyNum * 2 < liveNum && liveNum > pool->m_minNum) {
-            // 访问了线程池，需要加锁
-            lk.lock();
-            // 一次性销毁两个
-            pool->m_exitNum = NUMBER;
-            lk.unlock();
-            // 让工作线程自杀，无法做到直接杀死空闲线程，只能通知空闲线程让它自杀
-            for (int i = 0; i < NUMBER; ++i) {
-                pool->m_condition.notify_all();
-            }
-        }
-    }
-}
--- ThreadPool.h
+++ ThreadPool.h
@@ -11,45 +11,24 @@
 #include <vector>
 #include <condition_variable>
 #include <functional>
+#include <future>
 
-using namespace std;
-using namespace std::literals::chrono_literals;
-using callback = void(*)(void*);
-
-class Task {
-public:
-    callback m_function;
-    void* m_arg;
-    Task(callback func, void* arg) {
-        m_function = func;
-        m_arg = arg;
-    }
-};
 
 class ThreadPool {
 public:
-    ThreadPool(int min, int max);
+    ThreadPool(int num);
     ~ThreadPool();
 
-    void addTask(callback func, void* arg);
-    void addTask(Task task);
-    int busyNum();
-    int aliveNum();
-private:
-    queue<Task> m_taskQueue;        // 任务队列
-    thread m_managerID;             // 管理者线程ID
-    vector<thread> m_workThreads;   // 所有工作线程
-    int m_minNum;                   // 最小线程数
-    int m_maxNum;                   // 最大线程数
-    int m_busyNum;                  // 正在执行任务的线程数
-    int m_liveNum;                  // 存活的线程数
-    int m_exitNum;                  // 要销毁的线程数
+    template<class F, class... Args>
+          auto addTask(F&& f, Args&&... args) -> std::future<typename std::result_of<F(Args...)>::type>;
 
-    mutex m_mutexPool;              // 整个线程池的锁
-    condition_variable m_condition;      // 任务队列是否为空，阻塞工作者线程
-    bool m_shutdown;                // 是否销毁线程池
-    static void manager(void* arg);     // 管理者线程
-    static void worker(void* arg);      // 工作线程
+private:
+    std::queue<std::function<void()>> m_taskQueue;        // 任务队列
+    std::vector<thread> m_workThreads;   // 所有工作线程
+
+    mutex m_mutex;              // 整个线程池的锁
+    condition_variable m_condition;      // 条件变量，用于阻塞工作者线程
+    bool m_shutdown;                // 是否关闭线程池
 };
 
 
